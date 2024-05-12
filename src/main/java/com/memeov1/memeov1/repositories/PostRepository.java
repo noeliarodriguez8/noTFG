de294f9f2339eaf3e0a634ab1400b077df6ab44b
noeliarodriguez8 <noeliarodriguez888@hotmail.com>
2024-05-13 00:22:50 +0200

    crud post and feed ok

--- src/main/java/com/memeov1/memeov1/repositories/PostRepository.java
+++ src/main/java/com/memeov1/memeov1/repositories/PostRepository.java
@@ -3,19 +3,25 @@ package com.memeov1.memeov1.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.memeov1.memeov1.entities.Post;
+import com.memeov1.memeov1.entities.User;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
-    // listar posts (perfil e inicio)
-    List<Post> findPostsByUserUsername(String username); // en perfil
+    // listar posts (perfil)
+    List<Post> findPostsByUserUsername(String username);
 
-    List<Post> findPostsByOrderByCreatedDatetimeDesc(); // en inicio
-    // !!!!!!!! MIRAR EL ORDEN ¿AGE?¿ORDERBY? ¿¿¿¿????
+    // listar posts (feed)
+    @Query("SELECT p FROM Post p WHERE p.user IN (:following) ORDER BY p.createdDatetime DESC")
+    List<Post> findPostsByFollowingOrderByCreatedDatetimeDesc(@Param("following") List<User> following);
 
     // recuento posts (solo perfil) (en código)
 
     // recuento likes (en código)
+
+    public Post findByPostID(Integer postID);
 
 }
--- src/main/java/com/memeov1/memeov1/services/PostService.java
+++ src/main/java/com/memeov1/memeov1/services/PostService.java
@@ -1,16 +1,49 @@
 package com.memeov1.memeov1.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.memeov1.memeov1.entities.Follower;
+import com.memeov1.memeov1.entities.Post;
+import com.memeov1.memeov1.entities.User;
 import com.memeov1.memeov1.repositories.PostRepository;
+import com.memeov1.memeov1.repositories.UserRepository;
 
 @Service
 public class PostService {
 
     public final PostRepository postRepository;
+    public final UserRepository userRepository;
 
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Post create(Post post) {
+        return postRepository.saveAndFlush(post);
+    }
+
+    public List<Post> getPostsForCurrentUser(Integer userID) {
+        List<Follower> following = userRepository.findByUserID(userID).getFollowing();
+        List<User> toUserList = new ArrayList<>();
+        for (Follower follower : following) {
+            toUserList.add(follower.toUser);
+        }
+        return postRepository.findPostsByFollowingOrderByCreatedDatetimeDesc(toUserList);
+    }
+
+    // no sé si lo necesitamos
+    // public Post read(Integer postID) {
+    // return postRepository.findByPostID(postID);
+    // }
+
+    public String delete(Integer postID) {
+        Post post = postRepository.findByPostID(postID);
+        postRepository.deleteById(postID);
+        return "Deleted post: " + post.getPostID();
     }
 
 }
